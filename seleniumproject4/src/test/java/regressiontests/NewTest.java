1cc4c6cc1c26038020167180c0d5381580e96147
Lenovo <Lenovo@DESKTOP-I4ODGID>
2022-04-24 00:31:37 +0600

    Fifth Commit

---
+++ seleniumproject4/src/test/java/regressiontests/NewTest.java
@@ -0,0 +1,23 @@
+package regressiontests;
+
+import org.testng.annotations.Test;
+
+import commoncode.baseclass;
+import pages.fbhome;
+
+import org.testng.annotations.BeforeTest;
+
+import java.io.IOException;
+
+import org.testng.annotations.AfterTest;
+
+public class NewTest extends baseclass {
+  
+  @Test
+  public void f() throws IOException {
+	  fbhome ob = new fbhome(driver);
+	  ob.findfblinks();
+	
+  }
+}
+
